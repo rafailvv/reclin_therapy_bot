991729418b2bcd618b38fc18e9d25ab24067ca51
root <root@v2769264.hosted-by-vdsina.ru>
2025-07-18 22:28:22 +0300

    fix config.py

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     bot_token: str
     database_url: str
     chat_id: int
-    admin_ids: List[int] = [429272623]
+    admin_ids: List[int] = [933907011,734559231,1011594315,803910548,429272623]
     webapp_url: str
 
     model_config = SettingsConfigDict(
